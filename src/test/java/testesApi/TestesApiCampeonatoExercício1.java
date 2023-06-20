cc2f80c16079eccdcdbbb3fcdc2998f4cdad6181
Adriane Carvalho <adriane.carvalho@uol.com.br>
2023-06-20 10:50:13 -0300

    Ajuste no Exercício 1

--- src/test/java/testesApi/TestesApiCampeonatoExercício1.java
+++ src/test/java/testesApi/TestesApiCampeonatoExercício1.java
@@ -1,12 +1,15 @@
 package testesApi;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.containsString;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
 
 public class TestesApiCampeonatoExercício1 {
 
@@ -17,20 +20,20 @@ public class TestesApiCampeonatoExercício1 {
 		String token = "Bearer test_eade22cb669bacd6633dd23635cf65";
 
 		Map<String, String> headerMap = new HashMap<>();
-		String url = "https://api.api-futebol.com.br/v1";
-		String path = "/campeonatos";
+		String url = "https://api.api-futebol.com.br/v1/campeonatos";
 
 		headerMap.put("Authorization", token);
+		headerMap.put("charset", "UTF-8");
 
-		given()
+		Response response = given()
 				.headers(headerMap)
 				.log().all()
 				.when()
-				.get(url + path)
-				.then().assertThat()
-				.log().all()
-				.statusCode(200)
-				.body(containsString("Campeonato Brasileiro"));
+				.get(url);
+
+		List<String> nomePopularList = response.jsonPath().getList("edicao_atual.nome_popular");
+		String valorEsperado = "Brasileirão Série B 2023";
+		Assert.assertTrue(nomePopularList.contains(valorEsperado));
 
 	}
 
